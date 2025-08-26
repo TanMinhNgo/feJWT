156f5c334ead8464eece000662e3bb23131f7031
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-08-27 03:09:23 +0700

    Create Navigation and import Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import Nav from './components/Navigation/Nav'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Nav />
+      <h1>Welcome to React with Vite and TypeScript</h1>
+      <Routes >
+        <Route path="/"> Home </Route>
+        <Route path="/news"> News </Route>
+        <Route path="/contact"> Contact </Route>
+        <Route path="/about"> About </Route>
+        <Route path="*" element={<h2>404 Not Found</h2>} />
+      </Routes>
+    </div>
   )
 }
 
